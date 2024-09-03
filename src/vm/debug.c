5c96d53e5add9babcb0371e5e866c28c19dbc67d
msanlop <sanchez.lopez.mn@gmail.com>
2024-09-03 19:10:24 +0200

    add ints, add bytecode parsing for big endian

--- src/vm/debug.c
+++ src/vm/debug.c
@@ -5,83 +5,97 @@
 
 void debug_stack_print(VM* vm, char* str) {
     if(STACK_IS_EMPTY(vm)) {
-        DEBUG_PRINT("stack after push is empty\n");
+        DEBUG_PRINT("%s stack is EMPTY\n", str);
     }
     else {
-        DEBUG_PRINT("%s\n", str);
+        DEBUG_PRINT("%s\n" CYAN , str);
         for (Value* slot = vm->stack; slot < vm->stack_top; slot++) {
-            printf(YELLOW " %f\n" RESET, *slot);
+            PRINT_VALUE((*slot));
+            DEBUG_PRINT("\n")
         }
     }
-    DEBUG_PRINT("----------\n");
-
+    DEBUG_PRINT(RESET "----------\n");
 }
 
 void debug_constant_print(char* code_str, Chunk* chunk, int offset){
-    printf("%s    ", code_str);
+    DEBUG_PRINT("%s    ", code_str);
     int constant = GET_CODE_AT(chunk, offset);
-    Value val = chunk->constants.values[constant];
-    printf("'%.3lf'", val);
+    PRINT_VALUE(chunk->constants.values[constant]);
 }
 
 int debug_op_print(Chunk* chunk, int offset) {
-    printf("%04d  ", offset);
+    DEBUG_PRINT("%04d  ", offset);
     uint16_t line = GET_LINE_AT(chunk, offset);
     if(offset>0 && GET_LINE_AT(chunk, offset-1) == line) {
-        printf("    |  ");
+        DEBUG_PRINT("    |  ");
     } else {
-        printf("%5d  ", line);
+        DEBUG_PRINT("%5d  ", line);
     }
     
     switch (GET_CODE_AT(chunk, offset))
     {
     case OP_RETURN:
-        printf("OP_RETURN");
+        DEBUG_PRINT("OP_RETURN");
         return 1;
         break;
-    case OP_LOAD_CONST:
-        debug_constant_print("OP_LOAD_CONST", chunk, offset + 1);
+    case OP_DCONST:
+        debug_constant_print("OP_DCONST", chunk, offset + 1);
+        return 2;
+        break;
+    case OP_LCONST:
+        debug_constant_print("OP_LCONST", chunk, offset + 1);
         return 2;
         break;
     case OP_ADD:
-        printf("OP_ADD");
+        DEBUG_PRINT("OP_ADD");
         return 1;
         break;
     case OP_SUB:
-        printf("OP_SUB");
+        DEBUG_PRINT("OP_SUB");
         return 1;
     case OP_MUL:
-        printf("OP_MUL");
+        DEBUG_PRINT("OP_MUL");
         return 1;
     case OP_DIV:
-        printf("OP_DIV");
+        DEBUG_PRINT("OP_DIV");
         return 1;
         break;
     default:
         break;
     }
-    PRINT_ERR_ARGS("OP CODE NOT FOUND %d", GET_CODE_AT(chunk, offset));
+    PRINT_ERR_ARGS("ERROR %s:%d : op code not found %d",  __FILE__, __LINE__,
+        GET_CODE_AT(chunk, offset));
     exit(1);
 }
 
 void debug_chunk_print(Chunk* chunk) {
-    printf("/// Chunk ///\n");
+    DEBUG_PRINT("/// Chunk ///\n");
+    DEBUG_PRINT("/// constants ///\n");
+    for (size_t i=0; i < chunk->constants.count; i++) {
+        PRINT_VALUE(chunk->constants.values[i]);
+        DEBUG_PRINT("\n");
+    }
     for (size_t offset = 0; offset < chunk->count;)
     {
         int incr = debug_op_print(chunk, offset);
         offset+=incr;
-        printf("\n");
+        DEBUG_PRINT("\n");
     }
  }
 
 void debug_vm_dump(VM* vm) {
-    printf("///  VM   ///\n");
-    printf("ip: %p\n", vm->ip);
-    printf("/// stack ///\n");
-    printf("count: %d\n", (int) (vm->stack_top - vm->stack));
+    DEBUG_PRINT("\n");
+    DEBUG_PRINT("\n");
+    DEBUG_PRINT(RED "##### DUMP ####\n" RESET);
+    DEBUG_PRINT("///  VM   ///\n");
+    DEBUG_PRINT("ip: %p\n", vm->ip);
+    DEBUG_PRINT("/// stack ///\n");
+    DEBUG_PRINT("count: %d\n", (int) (vm->stack_top - vm->stack));
     debug_stack_print(vm, "values:");
     debug_chunk_print(&vm->chunk);
-    printf("/////////////\n");
+    DEBUG_PRINT(RED "/////////////\n" RESET);
+    DEBUG_PRINT("\n");
+    DEBUG_PRINT("\n");
 }
 
 #endif
