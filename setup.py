c50301bcae4cdb3801d4efe7c9deb1343f12f457
msanlop <sanchez.lopez.mn@gmail.com>
2024-10-31 01:18:45 +0100

    fix: idk

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from skbuild import setup
 from setuptools import find_packages
 
-# print(packages)
 
 setup(
     name="uza",
     version="0.0.1",
-    packages=["uza", "vm"],
+    packages=find_packages(),
     package_data={"uza": ["*.so", "*.so", "*.dll"], "vm": ["*.so", "*.so", "*.dll"]},
     include_package_data=True,
-    # packages=["uza", "vm"],
-    # cmake_install_dir="uza",
     cmake_install_dir="vm",
     cmake_args=["-DCMAKE_BUILD_TYPE=Release"],
     zip_safe=False,
