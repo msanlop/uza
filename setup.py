1a99bb11947af82b5d4f1258bb7d2d97f6623405
msanlop <sanchez.lopez.mn@gmail.com>
2025-02-03 15:16:30 +0100

    cd: fix build versions and distribute.yml

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from skbuild import setup
-from setuptools import find_packages
+import setuptools_scm
 
 setup(
     name="uza",
-    version="0.0.1",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     packages=["uzac", "vm"],
     package_data={"vm": ["*.so", "*.so", "*.dll"]},
     include_package_data=True,
